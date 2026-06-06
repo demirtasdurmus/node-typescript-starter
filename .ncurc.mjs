e712398ac42521664c8312e992df34ddf57d004b
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2026-06-06 11:25:40 +0300

    chore: add github action agent and replace ncu with depedabot (#21)

--- .ncurc.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('npm-check-updates').RcOptions } */
-export default {
-  upgrade: true,
-  target: 'minor',
-  packageManager: 'pnpm',
-  packageFile: 'package.json',
-  reject: ['pnpm'],
-};
